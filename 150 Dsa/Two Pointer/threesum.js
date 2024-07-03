b37e604b8d29c6c3ca69409c7031de0ebd0efc33
harimohan <harimohan.info@gmail.com>
2024-07-03 07:34:24 +0530

    3 threesum

---
+++ 150 Dsa/Two Pointer/threesum.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b); // Sort the array in ascending order
+    const result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // Skip duplicate elements
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++; // Skip duplicates
+                while (left < right && nums[right] === nums[right - 1]) right--; // Skip duplicates
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+};
+
+/*   
+Summary
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+
+*/
